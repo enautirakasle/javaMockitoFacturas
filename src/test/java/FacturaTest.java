6adaf8fd2ea42301c6e643f8a0c16e094d701679
ikaslea <ikaslea@ikaslea-SATELLITE-L735>
2022-10-24 19:43:39 +0200

    linea mockeatua ok

--- src/test/java/FacturaTest.java
+++ src/test/java/FacturaTest.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;  
+import static org.mockito.Mockito.when;  
+
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,6 +19,21 @@ public class FacturaTest {
 	    Factura f= new Factura(1,"compra online",lineas);
 	    
 	    assertEquals(264,f.getImporteConIVA(),0);
+	    
+	  }
+	  
+	  @Test
+	  public void testCalculoDelIva2() {
+	    
+	    LineaFactura l1= mock(LineaFactura.class);
+	    LineaFactura l2= mock(LineaFactura.class);
+	    when (l1.getImporteConIVA()).thenReturn(25.5);
+	    when (l2.getImporteConIVA()).thenReturn(30.3);
+	    
+	    List<LineaFactura> lineas= Arrays.asList(l1,l2);
+	    Factura f= new Factura(1,"compra online",lineas);
+	    
+	    assertEquals(55.8,f.getImporteConIVA(),0);
 	    
 	  }
 }
